0460f760c980b97a3e4994f085bb2aaff777b6b9
soumyo-jeet <samajdarsoumyajeet0@gmail.com>
2026-01-03 00:33:22 +0530

    daily updates added

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const { Server } = require("socket.io")
 const { createServer } = require("http")
 const cors = require("cors")
 const kafkaClient = require("./kafka_client")
+const initAdmin = require("./kafka_admin")
+const trigger_producer = require("./kafka_prods")
+const User = require("./models/User")
 require("dotenv").config()
 const PORT = process.env.PORT || 3000
 
@@ -22,9 +25,9 @@ const io = new Server(httpServer, {
 
 // kafka consumer
 const kafkaConsumerService = async () => {
-    const consumer = await kafkaClient.consumer({groupId: "temp"})
+    const consumer = await kafkaClient.consumer({ groupId: "temp" })
     console.log("Connecting Consumer")
-    consumer.connect()
+    await consumer.connect()
     console.log("connected Consumer")
 
     console.log("Consumer Subscribing topic")
@@ -33,13 +36,30 @@ const kafkaConsumerService = async () => {
 
 
     await consumer.run({
-        eachMessage: async ({topic, message}) => {
+        eachMessage: async ({ topic, message }) => {
             console.log(`Consumed from ${topic} | 
                 message: ${message.value.toString()}`)
+
+            const data = JSON.parse(message.value.toString())
+            const { longitude, latitude, advice } = data
+            const users = User.find({ lat: latitude, lon: longitude })
+            console.log(`lat: ${longitude}, lon: ${latitude}, adv: ${advice}`)
+        
+            if(!users) return
+            for (const user of users) {
+                user.advices.push({
+                    date: new Date(),
+                    message: advice
+                })
+                await user.save()
+            }
+
         }
     })
 }
 
+// initAdmin()
+// trigger_producer()
 kafkaConsumerService()
 
 app.get('/', (req, res) => {
---
+++ server/models/User.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose
+
+const adviceSchema = new Schema({
+    date: Date,
+    message: String
+})
+
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['user', 'ngo', 'admin'],
+    default: 'user'
+  },
+  /* ngo: {
+    // if the user is an NGO account, this references the NGO profile
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'NGO'
+  }, */
+  advices:{
+    type: [adviceSchema],
+  },
+  
+  lat: Number,
+  lon: Number,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  }
+  user.updatedAt = Date.now();
+  next();
+});
+
+// Compare password helper
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = model('User', userSchema);
